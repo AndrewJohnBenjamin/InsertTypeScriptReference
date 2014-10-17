7bb4e8e5dbee9db5caa069e13153a7cd15c55ac7
andrew.benjamin <andrew.benjamin@sigma-systems.com>
2014-10-17 15:45:11 +0100

    Created Solution
    The bare bones project with the insertion code added

---
+++ Insert Typescript Reference/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace TypescriptTools
+{
+    static class GuidList
+    {
+        public const string guidInsert_Typescript_ReferencePkgString = "3c4e6308-9f77-4358-89d8-73706b1544a8";
+        public const string guidInsert_Typescript_ReferenceCmdSetString = "1679fa06-40ec-473c-996c-4da2ea703e4e";
+
+        public static readonly Guid guidInsert_Typescript_ReferenceCmdSet = new Guid(guidInsert_Typescript_ReferenceCmdSetString);
+    };
+}
---
+++ Insert Typescript Reference/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace TypescriptTools
+{
+    static class PkgCmdIDList
+    {
+        public const uint InsertTypescriptReferenceID =        0x100;
+
+
+    };
+}
